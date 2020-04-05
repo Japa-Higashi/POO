8ef6ca60d55156e003b5672ae2987201d815c040
Japa-Higashi <62806853+Japa-Higashi@users.noreply.github.com>
2020-04-05 13:21:10 -0300

    Add files via upload

---
+++ EAD 26.03.2020/src/ead/pkg26/pkg03/pkg2020/EAD26032020.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ead.pkg26.pkg03.pkg2020;
+
+/**
+ *
+ * @author luizh
+ */
+public class EAD26032020 {
+
+    public static int soma(int n1, int n2){
+        return n1+n2;
+    }
+    public static void main(String[] args) {
+        int res;
+        
+        res = soma(3, 4);
+        System.out.println("A some é: " +res);
+    }
+    
+}
